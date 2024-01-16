fc7d994c6f3717f51e17cf12b39b7592deb315d3
guilhermeo97 <55219644+guilhermeo97@users.noreply.github.com>
2024-01-16 19:52:39 -0300

    v1 - teste

--- dio-java-basico/src/App.java
+++ dio-java-basico/src/App.java
@@ -1,5 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        int nome;
+        nome = 1;
+        System.out.println(nome);
     }
 }
